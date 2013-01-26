b5d144d734fa8608b1f546c0ea7afd494b77a943
Luke Carrier <luke@carrier.im>
2013-01-26 21:36:36 +0000

    Add Package Control and licensing to Sublime Text

--- chef/cookbooks/lukecarrier/attributes/default.rb
+++ chef/cookbooks/lukecarrier/attributes/default.rb
@@ -4,3 +4,5 @@ default['user']['homedir'] = '/home/luke'
 default['user']['login']   = 'luke'
 default['user']['group']   = 'luke'
 
+default['sublime_text']['license'] = 'http://deploy.lukecarrier.me/lukecarrier/sublime_text/License.sublime_license'
+
--- chef/cookbooks/lukecarrier/recipes/sublime_text.rb
+++ chef/cookbooks/lukecarrier/recipes/sublime_text.rb
@@ -14,3 +14,42 @@ end
 
 package 'sublime-text'
 
+script 'sublime-text --background' do
+  interpreter 'bash'
+  cwd         node['user']['homedir']
+  user        node['user']['login']
+  group       node['user']['group']
+
+  code <<-EOF
+    sublime-text --background &
+    sleep 2
+    pkill sublime_text
+  EOF
+
+  returns 0
+end
+
+config   = File.join(node['user']['homedir'], '.config', 'sublime-text-2')
+license  = File.join(config, 'Settings', 'License.sublime_license')
+packages = File.join(config, 'Installed Packages')
+
+remote_file File.join(packages, 'Package Control.sublime-package') do
+  action :create_if_missing
+  source 'http://sublime.wbond.net/Package%20Control.sublime-package'
+
+  owner node['user']['login']
+  group node['user']['group']
+  mode  0644
+end
+
+if node['sublime_text']['license'].length > 0
+  remote_file license do
+    action :create_if_missing
+    source node['sublime_text']['license']
+
+    user  node['user']['login']
+    group node['user']['group']
+    mode  0640
+  end
+end
+
