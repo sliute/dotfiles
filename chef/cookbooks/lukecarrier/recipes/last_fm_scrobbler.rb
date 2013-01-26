b4d0b854cb8c32e056d79ff1407e09340756693f
Luke Carrier <luke@carrier.im>
2013-01-26 19:30:29 +0000

    Add Last.fm scrobbler recipe

---
+++ chef/cookbooks/lukecarrier/recipes/last_fm_scrobbler.rb
@@ -0,0 +1,21 @@
+apt_repository 'lastfm' do
+  uri          'http://apt.last.fm/debian'
+  distribution 'testing'
+  components   ['main']
+
+  keyserver 'pgp.mit.edu'
+  key       'D49EEAA0'
+end
+
+execute 'apt-get update' do
+  command 'apt-get update >/dev/null'
+  returns 0
+end
+
+package 'lastfm-scrobbler'
+
+execute 'fix launcher path' do
+  command 'sed -ie \'s!^Exec=.*!Exec=/usr/bin/lastfm-scrobbler!\' /usr/share/applications/lastfm-client.desktop'
+  returns 0
+end
+
