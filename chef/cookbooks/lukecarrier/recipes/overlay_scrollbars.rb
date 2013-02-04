77879fd23597392f1c9e458c3aa9143c2c0f3737
Luke Carrier <luke@carrier.im>
2013-02-04 09:54:09 +0000

    Overlay scrollbars: kill them with fire

--- chef/cookbooks/lukecarrier/recipes/overlay_scrollbars.rb
+++ chef/cookbooks/lukecarrier/recipes/overlay_scrollbars.rb
@@ -1,4 +1,12 @@
-package 'overlay-scrollbars' do
+package 'overlay-scrollbar' do
+  action :purge
+end
+
+package 'overlay-scrollbar-gtk2' do
+  action :purge
+end
+
+package 'overlay-scrollbar-gtk3' do
   action :purge
 end
 
