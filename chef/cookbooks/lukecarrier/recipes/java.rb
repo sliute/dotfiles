ce5f2cc60d4cf7e4444f5329da41c988e2623ad0
Luke Carrier <luke@carrier.im>
2013-02-10 13:33:21 +0000

    Add Oracle Java recipe to base config

---
+++ chef/cookbooks/lukecarrier/recipes/java.rb
@@ -0,0 +1,25 @@
+apt_repository 'oracle-java' do
+  uri          'http://ppa.launchpad.net/webupd8team/java/ubuntu'
+  distribution node['lsb']['codename']
+  components   ['main']
+
+  keyserver 'keyserver.ubuntu.com'
+  key       'EEA14886'
+end
+
+execute 'apt-get update' do
+  command 'apt-get update >/dev/null'
+  returns 0
+end
+
+execute 'debconf shared/accepted-oracle-license-v1-1 select true' do
+  command 'echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections'
+  returns 0
+end
+
+execute 'debconf shared/accepted-oracle-license-v1-1 seen true' do
+  command 'echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections'
+  returns 0
+end
+
+package 'oracle-java7-installer'
