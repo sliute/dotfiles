6f0d5ca82efe6b14ffafde5eb37aea29689048bb
Luke Carrier <luke@carrier.im>
2013-09-07 16:30:53 +0100

    Host autocomplete for Mosh

--- chef/cookbooks/lukecarrier/recipes/mosh.rb
+++ chef/cookbooks/lukecarrier/recipes/mosh.rb
@@ -8,3 +8,11 @@ apt_repository 'mosh' do
 end
 
 package 'mosh'
+
+cookbook_file File.join(node['user']['homedir'], '.bash_profile.d', 'mosh') do
+  source 'bash_profile_mosh'
+
+  owner node['user']['login']
+  group node['user']['group']
+  mode  0644
+end
