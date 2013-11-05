07d0a8cefe2843bec15da25b7f63ec6cc7846cdd
Luke Carrier <luke@carrier.im>
2013-11-05 01:20:07 +0000

    Chef: remove Oracle VirtualBox repo until it gets a rebuild for 13.10

--- chef/cookbooks/lukecarrier/recipes/virtualbox.rb
+++ chef/cookbooks/lukecarrier/recipes/virtualbox.rb
@@ -1,16 +1,2 @@
-apt_repository 'oracle-vm-virtualbox' do
-  uri          'http://download.virtualbox.org/virtualbox/debian'
-  distribution node['lsb']['codename']
-  components   ['contrib']
-
-  keyserver 'pgp.mit.edu'
-  key       '98AB5139'
-end
-
-execute 'apt-get update' do
-  command 'apt-get update >/dev/null'
-  returns 0
-end
-
 package 'dkms'
-package 'virtualbox-4.2'
+package 'virtualbox'
