77952500539cc9535f9128cd1f510dda4e87a0c7
Luke Carrier <luke@carrier.im>
2013-11-05 01:19:01 +0000

    Chef: Mosh is now present in Ubuntu, no need for a PPA

--- chef/cookbooks/lukecarrier/recipes/mosh.rb
+++ chef/cookbooks/lukecarrier/recipes/mosh.rb
@@ -1,12 +1,3 @@
-apt_repository 'mosh' do
-  uri          'http://ppa.launchpad.net/keithw/mosh/ubuntu'
-  distribution node['lsb']['codename']
-  components   ['main']
-
-  keyserver 'keyserver.ubuntu.com'
-  key       '7BF6DFCD'
-end
-
 package 'mosh'
 
 cookbook_file File.join(node['user']['homedir'], '.bash_profile.d', 'mosh') do
