948e86006fc767b09682fd0a9f3e2fee942d6a23
Luke Carrier <luke@carrier.im>
2013-12-31 00:59:13 +0000

    Chef: git: fix ugly formatting

--- chef/cookbooks/lukecarrier/recipes/git.rb
+++ chef/cookbooks/lukecarrier/recipes/git.rb
@@ -1,11 +1,11 @@
 package 'git-all'
 
-template File.join(node['user']['homedir'], '.gitconfig') do 
+template File.join(node['user']['homedir'], '.gitconfig') do
   source 'gitconfig.erb'
-  variables({
-    :user_email   => node['user']['email'],
-    :user_homedir => node['user']['homedir'],
-    :user_name    => node['user']['name']
+  variables ({
+    user_email:   node['user']['email'],
+    user_homedir: node['user']['homedir'],
+    user_name:    node['user']['name'],
   })
 
   owner node['user']['login']
