fc2539cd2f93d9c97895a9e1fae484372f6cc520
Luke Carrier <luke@carrier.im>
2013-12-31 04:54:54 +0000

    Chef: xchat: automatically install and configure servers
    The servlist file is kept out of version control as it contains server
    passwords; alter your attributes to specify your own.

--- chef/cookbooks/lukecarrier/attributes/default.rb
+++ chef/cookbooks/lukecarrier/attributes/default.rb
@@ -8,3 +8,5 @@ default['deploy_baseurl']  = 'http://deploy.lukecarrier.me/lukecarrier'
 default['redshift']['config'] = "#{default['deploy_baseurl']}/redshift/redshift.conf"
 
 default['sublime_text']['license'] = "#{default['deploy_baseurl']}/sublime_text/License.sublime_license"
+
+default['xchat']['servlist'] = "#{default['deploy_baseurl']}/xchat/servlist.conf"
