6b87aaa940334188be66177e8a97d14bf11a8b31
Luke Carrier <luke@carrier.im>
2013-12-31 04:19:37 +0000

    Chef: gnome: prefer GTK3.0 dark theme

--- chef/cookbooks/lukecarrier/recipes/gnome.rb
+++ chef/cookbooks/lukecarrier/recipes/gnome.rb
@@ -1,4 +1,5 @@
-ppas = %w[gnome3 gnome3-next gnome3-staging]
+ppas          = %w[gnome3 gnome3-next gnome3-staging]
+gtk3_settings = File.join(node['user']['homedir'], '.config', 'gtk-3.0', 'settings.ini')
 
 ppas.each do |ppa|
   apt_repository ppa do
@@ -24,4 +25,12 @@ end
 execute 'apt-get dist-upgrade' do
   command 'apt-get dist-upgrade -y >/dev/null'
   returns 0
+end
+
+cookbook_file gtk3_settings do
+  source 'gnome_gtk3_settings.ini'
+
+  user  node['user']['login']
+  group node['user']['group']
+  mode  0640
 end
