2ac7a6175c2f2f5de028f484a9daf7fb5c8556d5
Luke Carrier <luke@carrier.im>
2014-01-03 20:26:59 +0000

    Chef: add missing metadata file to cookbook

---
+++ chef/cookbooks/lukecarrier/metadata.rb
@@ -0,0 +1,11 @@
+name              'lukecarrier'
+maintainer        'Luke Carrier'
+maintainer_email  'luke@carrier.im'
+license           'GPLv3'
+description       'Configures Ubuntu systems with no additional effort necessary'
+long_description  File.join(File.dirname(__FILE__), 'README.md')
+version           '0.0.1'
+
+supports 'ubuntu'
+
+depends 'apt'
