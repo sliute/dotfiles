5fc8093f3f8559c5479087d0207015da8daa0d16
Luke Carrier <luke@carrier.im>
2014-03-03 13:08:16 +0000

    No longer supports Ubuntu, no longer requires apt

--- cookbooks/lukecarrier/metadata.rb
+++ cookbooks/lukecarrier/metadata.rb
@@ -6,6 +6,4 @@ description       'Configures Ubuntu systems with no additional effort necessary
 long_description  File.join(File.dirname(__FILE__), 'README.md')
 version           '0.0.1'
 
-supports 'ubuntu'
-
-depends 'apt'
+supports 'fedora'
