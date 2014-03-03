65d7443055361e5dada52763fcb00234649ec027
Luke Carrier <luke@carrier.im>
2014-03-03 08:21:53 +0000

    Remove Ubuntu-specific kludge

--- cookbooks/lukecarrier/recipes/landscape.rb
+++
@@ -1,3 +0,0 @@
-package 'landscape-client-ui-install' do
-  action :purge
-end
--- cookbooks/lukecarrier/recipes/overlay_scrollbars.rb
+++
@@ -1,7 +0,0 @@
-packages = %w[overlay-scrollbar overlay-scrollbar-gtk2 overlay-scrollbar-gtk3]
-
-packages.each do |name|
-  package name do
-	  action :purge
-	end
-end
