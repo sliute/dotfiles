6998ecaa424ec2775da7298a91c07559b418d558
Luke Carrier <luke@carrier.im>
2014-03-03 13:12:14 +0000

    VirtualBox has to be installed from Oracle's own VM repos

--- cookbooks/lukecarrier/recipes/virtualbox.rb
+++ cookbooks/lukecarrier/recipes/virtualbox.rb
@@ -1,3 +1,12 @@
-packages = %w[dkms virtualbox]
+remote_file '/etc/yum.repos.d/virtualbox.repo' do
+  source 'http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo'
 
-packages.each {|name| package name}
+  owner 'root'
+  group 'root'
+  mode  0644
+end
+
+%w[dkms VirtualBox-4.3].each {|name| package name}
+
+execute 'service vboxdrv setup'
+execute "usermod -aG vboxusers #{node['user']['login']}"
