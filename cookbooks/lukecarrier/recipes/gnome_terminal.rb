e066479e27e738089adfd74756d7379e038badf4
Luke Carrier <luke@carrier.im>
2014-03-03 13:10:39 +0000

    Work around Gnome Terminal dconf bug
    Newer versions of Chef don't set up the environment correctly. Work
    around the stupid oversight by just using sudo.

--- cookbooks/lukecarrier/recipes/gnome_terminal.rb
+++ cookbooks/lukecarrier/recipes/gnome_terminal.rb
@@ -1,6 +1,3 @@
-# Assumes the presence of a Gnome Terminal version backed by dconf and not
-# gconf.
-
 dconf_dump = File.join(run_context.cookbook_collection[cookbook_name].root_dir,
                        'files', 'default', 'gnome_terminal')
 dconf_root = '/org/gnome/terminal'
@@ -8,12 +5,10 @@ dconf_root = '/org/gnome/terminal'
 script 'dconf reset -f && dconf load' do
   interpreter 'bash'
   cwd         node['user']['homedir']
-  user        node['user']['login']
-  group       node['user']['group']
 
   code <<-EOF
-    dconf reset -f #{dconf_root}/
-    dconf load     #{dconf_root}/ <#{dconf_dump}
+    sudo -u #{node['user']['login']} dconf reset -f #{dconf_root}/
+    sudo -u #{node['user']['login']} dconf load     #{dconf_root}/ <#{dconf_dump}
   EOF
 
   returns 0
