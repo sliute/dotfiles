9f0904440d1d0269b53d67f7c8b17248d3554ac6
Luke Carrier <luke@carrier.im>
2014-04-19 23:29:31 +0100

    Verify SSL certs, don't whine

--- solo.rb
+++ solo.rb
@@ -4,3 +4,5 @@ file_cache_path root
 cookbook_path   File.join(root, 'cookbooks')
 recipe_path     File.join(root, 'recipes')
 role_path       File.join(root, 'roles')
+
+ssl_verify_mode :verify_peer
