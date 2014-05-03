eab5ea763a00a33b3bf3fe857eae96507ccbc163
Luke Carrier <luke@carrier.im>
2014-05-04 00:23:14 +0100

    node_development: forbid npm from barfing packages everywhere

--- cookbooks/lukecarrier/recipes/node_development.rb
+++ cookbooks/lukecarrier/recipes/node_development.rb
@@ -1,2 +1,40 @@
 package 'nodejs'
 package 'npm'
+
+node_env = { "HOME" => node['user']['homedir'],
+             "USER" => node['user']['login'] }
+
+execute 'npm config set cache' do
+  command "npm config set cache #{node['user']['homedir']}/.npm/cache"
+
+  environment node_env
+
+  user  node['user']['login']
+  group node['user']['group']
+end
+
+execute 'npm config set prefix' do
+  command "npm config set prefix #{node['user']['homedir']}/.npm/prefix"
+
+  environment node_env
+
+  user  node['user']['login']
+  group node['user']['group']
+end
+
+execute 'npm config set tmp' do
+  command "npm config set tmp #{node['user']['homedir']}/.npm/tmp"
+
+  environment node_env
+
+  user  node['user']['login']
+  group node['user']['group']
+end
+
+cookbook_file File.join(node['user']['homedir'], '.bash_profile.d', 'npm') do
+  source 'bash_profile_npm'
+
+  owner node['user']['login']
+  group node['user']['group']
+  mode  0644
+end
