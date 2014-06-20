f6f95b578d66ba2eaa7b29616d142fcb3216fc01
Luke Carrier <luke@carrier.im>
2014-06-20 19:41:03 +0100

    Added Cheffile template

--- solo.rb
+++ solo.rb
@@ -1,7 +1,10 @@
 root = File.expand_path(File.dirname(__FILE__))
 
 file_cache_path root
-cookbook_path   File.join(root, 'cookbooks')
+cookbook_path   [
+  File.join(root, 'cookbooks'),
+  File.join(root, 'site-cookbooks'),
+]
 recipe_path     File.join(root, 'recipes')
 role_path       File.join(root, 'roles')
 
