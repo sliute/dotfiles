d5652a05d9bc045b8f2b57d2146fee53c3dae322
Luke Carrier <luke@carrier.im>
2014-10-03 20:47:11 +0100

    .: set cache dir

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
 root = File.expand_path(File.dirname(__FILE__))
 
-file_cache_path root
+file_cache_path File.join(root, 'cache')
 cookbook_path   File.join(root, 'cookbooks'), File.join(root, 'site-cookbooks')
 recipe_path     File.join(root, 'recipes')
 role_path       File.join(root, 'roles')
