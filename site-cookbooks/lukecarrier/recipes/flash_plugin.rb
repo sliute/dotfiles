a45516fbb155f0fc27f29f6de7a4aba5f2e234a4
Luke Carrier <luke@carrier.im>
2014-10-03 20:47:48 +0100

    flash_plugin: new!

---
+++ site-cookbooks/lukecarrier/recipes/flash_plugin.rb
@@ -0,0 +1,9 @@
+rpm_path = "#{Chef::Config[:file_cache_path]}/flash_plugin-adobe_release.rpm"
+
+remote_file rpm_path do
+  source 'http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm'
+end
+
+rpm_package 'adobe-release' do
+  source rpm_path
+end
