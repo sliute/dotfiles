02d9b9cd395b0423f3102a8baa3426f03a3926e7
Luke Carrier <luke@carrier.im>
2015-04-20 22:56:49 +0100

    owncloud_client: apparently in F21's repos as Mirall now

--- site-cookbooks/lukecarrier/recipes/owncloud_client.rb
+++ site-cookbooks/lukecarrier/recipes/owncloud_client.rb
@@ -1,5 +1,2 @@
-remote_file '/etc/yum.repos.d/isv:ownCloud:desktop.repo' do
-  source 'http://download.opensuse.org/repositories/isv:ownCloud:desktop/Fedora_20/isv:ownCloud:desktop.repo'
-end
+package 'mirall'
 
-package 'owncloud-client'
