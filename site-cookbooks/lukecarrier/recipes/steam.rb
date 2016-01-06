84c733105334ba7c2a10c5b558e1d05b7183f6ad
Luke Carrier <luke@carrier.im>
2016-01-06 21:13:07 +0000

    steam: fix for Fedora 21 and 22

--- site-cookbooks/lukecarrier/recipes/steam.rb
+++ site-cookbooks/lukecarrier/recipes/steam.rb
@@ -1,3 +1,11 @@
 include_recipe 'lukecarrier::rpmfusion_repo'
 
+remote_file '/etc/yum.repos.d/negativo17-steam.repo' do
+  source 'http://negativo17.org/repos/fedora-steam.repo'
+
+  owner 'root'
+  group 'root'
+  mode   0644
+end
+
 package 'steam'
