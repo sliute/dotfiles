39a5300e3c58081898297884110ee4dd731c47a3
Luke Carrier <luke@carrier.im>
2016-02-07 10:21:18 +0000

    steam: use the recipe!

--- cookbooks/lukecarrier/recipes/steam.rb
+++ cookbooks/lukecarrier/recipes/steam.rb
@@ -1,11 +1,3 @@
-include_recipe 'lukecarrier::rpmfusion_repo'
+# Steam client
 
-remote_file '/etc/yum.repos.d/negativo17-steam.repo' do
-  source 'http://negativo17.org/repos/fedora-steam.repo'
-
-  owner 'root'
-  group 'root'
-  mode   0644
-end
-
-package 'steam'
+include_recipe 'steam'
