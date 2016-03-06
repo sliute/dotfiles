cabce60e7e3690494ed80345b517202c55e655b6
Luke Carrier <luke@carrier.im>
2016-03-06 21:53:44 +0000

    bash: update profile configuration for Ubuntu

--- cookbooks/lukecarrier/recipes/bash.rb
+++ cookbooks/lukecarrier/recipes/bash.rb
@@ -1,39 +1,26 @@
-configs = %w[editor history path prompt_command title]
+# Bash profile
 
 cookbook_file File.join(node['user']['homedir'], '.bash_profile') do
   owner node['user']['login']
   group node['user']['group']
   mode  0644
 
-  source 'bash_profile'
+  source 'bash/profile'
 end
 
-directories = %w[.bash_profile.d .local/bin]
-directories.map! {|current_directory| File.join(node['user']['homedir'], current_directory)}
-directories << node['user']['applicationdir']
-
-directories.each do |current_directory|
-  directory current_directory do
-    owner node['user']['login']
-    group node['user']['group']
-    mode  0755
-  end
-end
-
-file File.join(node['user']['homedir'], '.bash_profile.d', '_') do
-  action :touch
-
+directory File.join(node['user']['homedir'], '.bash_profile.d') do
   owner node['user']['login']
   group node['user']['group']
-  mode  0644
+  mode  0755
 end
 
+configs = %w[debian editor prompt_command title]
 configs.each do |config|
   cookbook_file File.join(node['user']['homedir'], '.bash_profile.d', config) do
     owner node['user']['login']
     group node['user']['group']
     mode  0644
 
-    source "bash_profile_#{config}"
+    source "bash/profile_#{config}"
   end
 end
