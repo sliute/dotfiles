bb71cced7dcf580880754f942892e96d248a1017
Luke Carrier <luke@carrier.im>
2016-03-07 16:11:30 +0000

    atom: more nice things

--- cookbooks/lukecarrier/recipes/atom.rb
+++ cookbooks/lukecarrier/recipes/atom.rb
@@ -4,9 +4,12 @@ include_recipe 'atom'
 package 'atom'
 
 atom_packages = [
+  'atom-ctags',
+  'autocomplete-clang',
   'editorconfig',
-  'language-powershell',
+  'language-cmake', 'language-glsl', 'language-powershell',
   'linter', 'linter-alex',
   'minimap',
+  'Zen',
 ]
 atom_packages.each {|atom_package| atom_apm atom_package}
