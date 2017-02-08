d3f8cf01f595e1f6b1f37a764dc8a1831d742d11
Luke Carrier <luke@carrier.im>
2017-02-08 17:33:59 +0000

    gnome_terminal: fix dconf load

--- cookbooks/lukecarrier/recipes/gnome_terminal.rb
+++ cookbooks/lukecarrier/recipes/gnome_terminal.rb
@@ -1,16 +1,17 @@
 # Gnome Terminal
 
-dconf_dump = File.join(run_context.cookbook_collection[cookbook_name].root_dir,
-                       'files', 'default', 'gnome_terminal')
+#puts run_context.node.to_yaml
+#puts run_context.cookbook_collection[cookbook_name].to_yaml
+
+cookbook   = run_context.cookbook_collection[cookbook_name]
+dconf_dump = cookbook.preferred_filename_on_disk_location(run_context.node, :files, 'gnome_terminal')
 dconf_root = '/org/gnome/terminal'
 
 bash 'dconf reset -f && dconf load' do
   cwd node['user']['homedir']
 
   code <<-EOF
-    echo $USER
-
-    su -c 'dconf reset -f #{dconf_root}/'                #{node['user']['login']}
-    su -c 'dconf load     #{dconf_root}/ <#{dconf_dump}' #{node['user']['group']}
+    su -c 'dconf reset -f #{dconf_root}/' #{node['user']['login']}
+    su -c 'cat #{dconf_dump} | dconf load #{dconf_root}/' #{node['user']['group']}
   EOF
 end
