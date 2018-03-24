8b56b00d21f2520b5a721522605bc5fa25d68ba2
Luke Carrier <luke@carrier.im>
2018-03-24 22:40:16 +0000

    docker: improve cleanup funcs

--- docker/shrc.d/docker.sh
+++ docker/shrc.d/docker.sh
@@ -1,4 +1,9 @@
 docker_cleanup() {
-    docker ps -a | tail -n +2 | awk '{print $1}' | xargs -r docker rm
-    docker images | tail -n +2 | grep '^<none>' | awk '{print $3}' | xargs -r docker rmi
+    docker ps -aq | xargs -r docker rm
+    docker images -q | grep '^<none>' | awk '{print $3}' | xargs -r docker rmi
+}
+
+docker_cleanup_all() {
+    docker ps -aq | xargs -r docker rm
+    docker images -q | xargs -r docker rmi
 }
